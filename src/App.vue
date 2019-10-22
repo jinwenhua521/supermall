afdfad84d873bdb2339aa8051111387122810d92
Lazzaro <734701641@qq.com>
2019-10-22 15:17:24 +0800

    更新目录结构

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,6 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "assets/css/base.css";
 }
 </style>
